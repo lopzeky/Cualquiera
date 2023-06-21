ef077c640def91410bf0246f056f982f3b4781ff
Ezequiel Gabriel López Ayavire <e.lopez35@alumnos.santotomas.cl>
2023-06-21 15:32:10 -0400

    Validacion-Nombre-Apellido-Gmail=als@"AceptaTodoDespuesDel@"

--- Cualquiera/Models/Medico.cs
+++ Cualquiera/Models/Medico.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Cualquiera.Models;
 
@@ -7,7 +8,12 @@ public partial class Medico
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "El campo Nombres es obligatorio.")]
+    [RegularExpression(@"^[A-Za-z ]+$", ErrorMessage = "El campo Nombres solo puede contener letras.")]
     public string Nombres { get; set; } = null!;
+
+    [Required(ErrorMessage = "El campo Apellidos es obligatorio.")]
+    [RegularExpression(@"^[A-Za-z ]+$", ErrorMessage = "El campo Apellidos solo puede contener letras.")]
 
     public string Apellidos { get; set; } = null!;
 
@@ -15,6 +21,8 @@ public partial class Medico
 
     public string Rut { get; set; } = null!;
 
+    [Required(ErrorMessage = "El campo Email es obligatorio.")]
+    [EmailAddress(ErrorMessage = "El campo Email no tiene un formato válido.")]
     public string Email { get; set; } = null!;
 
     public bool Disponible { get; set; }
