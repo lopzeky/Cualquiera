df43ec8ad2ffd112c568a37cb19b2f91bd8e9217
ConejoCaco <huanca1992mamani@gmail.com>
2023-06-30 04:45:30 -0400

    traer datos a consola

---
+++ Cualquiera/wwwroot/js/generandoxpdf.js
@@ -0,0 +1,22 @@
+﻿function impr() {
+    var tabla = document.getElementsByTagName("tr");
+    for (var i = 1; i < tabla.length; i++) {
+        var nombre = tabla[i].getElementsByTagName("td")[0].textContent;
+        var apellidos = tabla[i].getElementsByTagName("td")[1].textContent;
+        var fechanac = tabla[i].getElementsByTagName("td")[2].textContent;
+        var rut = tabla[i].getElementsByTagName("td")[3].textContent;
+        var email = tabla[i].getElementsByTagName("td")[4].textContent;
+
+        console.log("Nombre:", nombre);
+        console.log("Apellidos:", apellidos);
+        console.log("Fecha de Nacimiento:", fechanac);
+        console.log("RUT:", rut);
+        console.log("Email:", email);
+
+        
+
+
+    }
+}
+
+document.getElementById("botoncito").addEventListener("click", impr);
